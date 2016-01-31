e5a926dea4f4e61c456dcc80fb2ff659bb2a87ab
HsoulT <alxanderr@gmail.com>
2016-01-31 04:14:17 -0400

    Added unhighlighting
    Also fixed having to double click for proper player position

--- Source/Ritual_Puzzle/Amazon_Levels.h
+++ Source/Ritual_Puzzle/Amazon_Levels.h
@@ -12,16 +12,11 @@ UCLASS()
 class RITUAL_PUZZLE_API UAmazon_Levels : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
-		UFUNCTION(BlueprintCallable, Category = "AlexSucks")
 
-public:
+	UFUNCTION(BlueprintCallable, Category = "AlexSucks")
 	static TArray<int> GetAmazonLevel(int level_number, int &n_count);
 	
-private:
 	UFUNCTION(BlueprintCallable, Category = "AlexSucks")
-
-public:
 	static TArray<int> GetAmazonMoves(TArray <int> Grid, int dim, int pawn_index);
 	
 };
